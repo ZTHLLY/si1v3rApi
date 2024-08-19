955a60f9cdd9274d93c8197ca8e45f80612a03ac
ZTHLLY <zzz18011940139@gmail.com>
2024-08-19 17:31:12 +0800

    virtualApi interface update

--- virtualApi/src/main/java/com/si1v3r/virtualapi/clint/Si1v3rApiclint.java
+++ virtualApi/src/main/java/com/si1v3r/virtualapi/clint/Si1v3rApiclint.java
@@ -2,7 +2,13 @@ package com.si1v3r.virtualapi.clint;
 
 
 import java.util.HashMap;
+
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
 import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONUtil;
+import com.si1v3r.virtualapi.model.User;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public class Si1v3rApiclint {
 
@@ -10,7 +16,27 @@ public class Si1v3rApiclint {
         //可以单独传入http参数，这样参数会自动做URL编码，拼接在URL中
         HashMap<String, Object> paramMap = new HashMap<>();
         paramMap.put("name", name);
-        String result = HttpUtil.get("http://localhost:8002/api/name", paramMap);
+        String result = HttpUtil.get("http://localhost:8002/api/name/", paramMap);
+        System.out.println(result);
+        return result;
+    }
+
+    public String getNameByPost(String name) {
+        //可以单独传入http参数，这样参数会自动做URL编码，拼接在URL中
+        HashMap<String, Object> paramMap = new HashMap<>();
+        paramMap.put("name", name);
+        String result = HttpUtil.post("http://localhost:8002/api/name/", paramMap);
+        System.out.println(result);
+        return result;
+    }
+
+    public String getUsernameByPost(@RequestBody User user) {
+        String json = JSONUtil.toJsonStr(user);
+        HttpResponse httpResponse = HttpRequest.post("http://localhost:8002/api/name/user")
+                .body(json)
+                .execute();
+        System.out.println(httpResponse.getStatus());
+        String result = httpResponse.body();
         System.out.println(result);
         return result;
     }
--- virtualApi/src/test/java/com/si1v3r/virtualapi/VirtualApiApplicationTests.java
+++ virtualApi/src/test/java/com/si1v3r/virtualapi/VirtualApiApplicationTests.java
@@ -1,6 +1,7 @@
 package com.si1v3r.virtualapi;
 
 import com.si1v3r.virtualapi.clint.Si1v3rApiclint;
+import com.si1v3r.virtualapi.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,9 +14,19 @@ class VirtualApiApplicationTests {
 
     @Test
     void clintTest(){
+        String params="ruiming";
+        User user1 = new User();
+        user1.setName(params);
+
+
         Si1v3rApiclint si1v3rApiclint=new Si1v3rApiclint();
-        String result = si1v3rApiclint.getNameByGet("si1v3r");
+        String result = si1v3rApiclint.getNameByGet(params);
+        String result2 = si1v3rApiclint.getNameByPost(params);
+        String result3 = si1v3rApiclint.getUsernameByPost(user1);
+
         System.out.println(result);
+        System.out.println(result2);
+        System.out.println(result3);
     }
 
 }
