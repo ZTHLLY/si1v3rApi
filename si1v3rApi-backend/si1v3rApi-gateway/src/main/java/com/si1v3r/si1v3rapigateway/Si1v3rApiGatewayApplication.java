748d781acc0e10724747d6ce75c7436ecd5d8a1e
ZTHLLY <zzz18011940139@gmail.com>
2024-10-21 21:22:34 +0800

    add provider

--- si1v3rApi-backend/si1v3rApi-gateway/src/main/java/com/si1v3r/si1v3rapigateway/Si1v3rApiGatewayApplication.java
+++ si1v3rApi-backend/si1v3rApi-gateway/src/main/java/com/si1v3r/si1v3rapigateway/Si1v3rApiGatewayApplication.java
@@ -1,26 +1,54 @@
 package com.si1v3r.si1v3rapigateway;
 
+import com.si1v3r.project.provider.DemoService;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Service;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class})
+@EnableDubbo
+@Service
 public class Si1v3rApiGatewayApplication {
 
+    @DubboReference
+    private DemoService demoService;
+
     public static void main(String[] args) {
-        SpringApplication.run(Si1v3rApiGatewayApplication.class, args);
+
+        ConfigurableApplicationContext context = SpringApplication.run(Si1v3rApiGatewayApplication.class, args);
+        Si1v3rApiGatewayApplication application = context.getBean(Si1v3rApiGatewayApplication.class);
+        String result = application.doSayHello("world");
+        String result2 = application.doSayHello2("world");
+        System.out.println("result: " + result);
+        System.out.println("result: " + result2);
     }
 
-        @Bean
-        public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-            return builder.routes()
-                    .route("toBaidu", r -> r.path("/baidu")
-                            .uri("http://www.baidu.com"))
-                    .route("tosi1v3rApi", r -> r.host("*.myhost.org")
-                            .uri("http://httpbin.org"))
-                    .build();
-        }
+    public String doSayHello(String name) {
+        return demoService.sayHello(name);
+    }
+
+    public String doSayHello2(String name) {
+        return demoService.sayHello2(name);
+    }
+
+    //    @Bean
+    //    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+    //        return builder.routes()
+    //                .route("tobaidu", r -> r.path("/baidu")
+    //                        .uri("https://www.baidu.com"))
+    //                .route("toyupiicu", r -> r.path("/yupiicu")
+    //                        .uri("http://yupi.icu"))
+    //                .build();
+    //    }
 
 }
+
